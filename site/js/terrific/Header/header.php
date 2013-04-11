46562cef7a07132ad01bdd9849d54396b5f9302d
Patrick Roßnagel <paddyrossnagel@aol.com>
2013-04-11 18:20:24 +0200

    Fixed CSS Paths (funzt noch nicht, warum?)

--- site/js/terrific/Header/header.php
+++ site/js/terrific/Header/header.php
@@ -10,34 +10,32 @@
 		<title>Seapal</title>
 		
 		<!-- Favicon -->
-		<link rel='icon' type='image/png' href='../img/icons/seapal_normal.png'/>
+		<link rel='icon' type='image/png' href='../../../../img/icons/seapal_normal.png'/>
 		
 		<!-- Styles -->
-		<link href="../lib/bootstrap/css/bootstrap.css" rel="stylesheet">
-		<link href="../lib/bootstrap/css/layout.css" rel="stylesheet">
-		<link href="../lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="../../../../lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="../../../../lib/bootstrap/css/layout.css" rel="stylesheet">
+		<link href="../../../../lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 		
 		<!-- Custom CSS -->
-		<link href="css/style.css" rel="stylesheet">
+		<link href="../../../css/style.css" rel="stylesheet">
 		
 		<!-- Custom Module CSS -->
-		<link href="js/terrific/Footer/css/footer.css" rel="stylesheet">
-		<link href="js/terrific/Header/css/header.css" rel="stylesheet">
+		<link href="css/header.css" rel="stylesheet">
+		<link href="../Footer/css/footer.css" rel="stylesheet">
 		
-		
-		
-		<script type="text/javascript" src="../lib/js/jquery.js"></script>
-        <script type="text/javascript" src="../lib/js/jquery-ui.js"></script>
-        <script type="text/javascript" src="../lib/js/terrific-2.0.1.js"></script>
-        <script type="text/javascript" src="../lib/bootstrap/js/bootstrap.js"></script>
+		<script type="text/javascript" src="../../../../lib/js/jquery.js"></script>
+        <script type="text/javascript" src="../../../../lib/js/jquery-ui.js"></script>
+        <script type="text/javascript" src="../../../../lib/js/terrific-2.0.1.js"></script>
+        <script type="text/javascript" src="../../../../lib/bootstrap/js/bootstrap.js"></script>
 		
 		<script type="text/javascript">
 			$("body").ready(function(){
-				$('#footer').load('js/terrific/Footer/footer.html');
-				$('#header').load('js/terrific/Header/header.html');
+				$('#footer').load('../Footer/footer.php');
+				$('#header').load('header.html');
 			});
 		</script>
 		
-		<script type="text/javascript" src="js/static/main.js"></script>
+		<script type="text/javascript" src="../../static/main.js"></script>
 		
 	</head>
