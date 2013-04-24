59c5bb3c90c33be6eda0ea03a13eaa1fe53c3a35
Jakub Werner <jack@ubilix.com>
2013-04-24 14:56:53 +0200

    markup clean up

--- site/app_waypoint.php
+++ site/app_waypoint.php
@@ -9,92 +9,91 @@
 
 		
 		<!-- Container -->
-		<div class="container-fluid">
+		<div class="container">
 						
 			<!-- Content -->		
-			<div id="appWrapper" align="center">
-			    <br />
+			<div id="appWrapper">
+			
 			    <h2>Wegpunkt</h2>
-			    <br />
-			    <div class="container-fluid" align="center">
-	            	<form class="form-horizontal"> 
-		            	<div class="row well" style="margin-left: 15%;" align="center">
-		            		<div class="span4" align="center">	            		
-			            		<div class="control-group">
-			            			<label class="control-label">Name</label>
-			            			<input class="input-medium" type="text" id="name"/>
-			            		</div>
-			            		<div class="control-group">
-			            			<label class="control-label">Time</label>
-			            			<input class="input-medium" type="date" id="wdate"/>
-			                    </div>
-			                    <div class="control-group">
-			            			<label class="control-label">Date</label>
-			            			<input class="input-medium" type="date" id="wtime"/>
-			                    </div>
-		            		</div>
-		            		<div class="span4">
-		            			<div class="control-group">
-			            			<label class="control-label">Latitude</label>
-			            			<input class="input-medium" type="text" id="lat"/>
-			            		</div>
-			            		<div class="control-group">
-			            			<label class="control-label">Longitude</label>
-			            			<input class="input-medium" type="text" id="lng"/>
-			                    </div>
-			                    <div class="control-group">
-			            			<label class="control-label">Fahrt nach</label>
-			            			<select name="fahrtziel" id="marker" style="width: 165px;"></select>
-			                    </div>
-		            		</div>
-		            		<div class="span4">
-		            			<div class="control-group">
-			            			<label class="control-label">COG</label>
-			            			<input class="input-medium" type="text" id="cog"/>
-			                    </div>
-			                    <div class="control-group">
-			            			<label class="control-label">SOG</label>
-			            			<input class="input-medium" type="text" id="sog"/>
-			                    </div>
-			                    
-			                    <div class="control-group">
-			                    	<label class="control-label">Manoever</label>
-			            			<select name="manoever" id="manoever" style="width: 165px;"></select>
-			                    </div>                   
-		            		</div>
-		            		<div class="span4">
-		            			<div class="control-group">
-			            			<label class="control-label">BTM</label>
-			            			<input class="input-medium" type="text" id="btm"/>
-			                    </div>
-			                    <div class="control-group">
-			            			<label class="control-label">DTM</label>
-			            			<input class="input-medium" type="text" id="dtm"/>
-			                    </div>
-			                    <div class="control-group">
-			            			<label class="control-label">Vorsegel</label>
-			            			<select name="vorsegel" id="vorsegel" style="width: 165px;"></select>
-			            		</div>
-		            		</div>
-		            	</div>      	 
-	            	</div>
-	            </div>
-			    <br />
-			    <br />
+			    
+            	<form class="form-horizontal row well"> 
+            	
+            		<div class="span5">	            		
+	            		<div class="control-group">
+	            			<label class="control-label">Name</label>
+	            			<input class="input-medium" type="text" id="name"/>
+	            		</div>
+	            		<div class="control-group">
+	            			<label class="control-label">Time</label>
+	            			<input class="input-medium" type="date" id="wdate"/>
+	                    </div>
+	                    <div class="control-group">
+	            			<label class="control-label">Date</label>
+	            			<input class="input-medium" type="date" id="wtime"/>
+	                    </div>
+            		</div>
+            		
+            		<div class="span5">
+            			<div class="control-group">
+	            			<label class="control-label">Latitude</label>
+	            			<input class="input-medium" type="text" id="lat"/>
+	            		</div>
+	            		<div class="control-group">
+	            			<label class="control-label">Longitude</label>
+	            			<input class="input-medium" type="text" id="lng"/>
+	                    </div>
+	                    <div class="control-group">
+	            			<label class="control-label">Fahrt nach</label>
+	            			<select name="fahrtziel" id="marker" ></select>
+	                    </div>
+            		</div>
+            		
+            		<div class="span5">
+            			<div class="control-group">
+	            			<label class="control-label">COG</label>
+	            			<input class="input-medium" type="text" id="cog"/>
+	                    </div>
+	                    <div class="control-group">
+	            			<label class="control-label">SOG</label>
+	            			<input class="input-medium" type="text" id="sog"/>
+	                    </div>
+	                    
+	                    <div class="control-group">
+	                    	<label class="control-label">Manoever</label>
+	            			<select name="manoever" id="manoever"></select>
+	                    </div>                   
+            		</div>
+            		
+            		<div class="span5">
+            			<div class="control-group">
+	            			<label class="control-label">BTM</label>
+	            			<input class="input-medium" type="text" id="btm"/>
+	                    </div>
+	                    <div class="control-group">
+	            			<label class="control-label">DTM</label>
+	            			<input class="input-medium" type="text" id="dtm"/>
+	                    </div>
+	                    <div class="control-group">
+	            			<label class="control-label">Vorsegel</label>
+	            			<select name="vorsegel" id="vorsegel"></select>
+	            		</div>
+            		</div>
+            	</form>
 			    <div class="container" align="center">
-			    <div class="row" style="margin-left:5%;">
-			        <div class="span4" id="appNotes">
-			        	<h4>Notes</h4>
-			            <textarea style="width:96%; height:360px;"></textarea>
-			        </div>
-			        <div class="span4" id="markerMap">
-			        	<h4>Map</h4>
-			            <img src="../img/icons/marker_map.png" id="appInfoPhoto" style="width:100%; heigt: 100%;"/>
-			        </div>
-			        <div class="span4" id="appPhotos">
-			        	<h4>Photos</h4>
-				        <img src="../img/icons/no_image.jpg" id="appInfoPhoto" style="width:100%; heigt: 100%;"/>
-				    </div>
+				    <div class="row">
+				        <div class="span4" id="appNotes">
+				        	<h4>Notes</h4>
+				            <textarea></textarea>
+				        </div>
+				        <div class="span4" id="markerMap">
+				        	<h4>Map</h4>
+				            <img src="../img/icons/marker_map.png" id="appInfoPhoto"/>
+				        </div>
+				        <div class="span4" id="appPhotos">
+				        	<h4>Photos</h4>
+					        <img src="../img/icons/no_image.jpg" id="appInfoPhoto"/>
+					    </div>
+					</div>
 				</div>
 			</div><!-- Content -->
 			
