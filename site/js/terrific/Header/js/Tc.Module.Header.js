60f3addd642fd5bc2ed218c0592e667c071ae352
Jakub Werner <jack@ubilix.com>
2013-04-25 13:44:26 +0200

    comment

--- site/js/terrific/Header/js/Tc.Module.Header.js
+++ site/js/terrific/Header/js/Tc.Module.Header.js
@@ -2,16 +2,12 @@
   Tc.Module.Header = Tc.Module.extend({
     on: function(callback) {
     	var self = this;
-    	console.log(window.location);
+    	
+    	
+    	// Adding active class per javascript to the header
     	phpfilesplit = window.location.pathname.split("/");
     	phpfile = phpfilesplit[phpfilesplit.length - 1];
-    	console.log(phpfile);
     	$("a[href*='"+phpfile+"']",self.$ctx).parents("li").addClass("active");
-    	
-    	
-    	
-    	
-    	
     	
         callback();
     },
