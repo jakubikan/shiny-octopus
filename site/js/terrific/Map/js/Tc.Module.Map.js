982101a61b4b09e053265adef68fca8d075212f2
Patrick Roßnagel <paddyrossnagel@aol.com>
2013-05-01 23:53:13 +0200

    Crosshair. Routes draggable. map.html erweitert.

--- site/js/terrific/Map/js/Tc.Module.Map.js
+++ site/js/terrific/Map/js/Tc.Module.Map.js
@@ -2,10 +2,7 @@
   Tc.Module.Map = Tc.Module.extend({
     on: function(callback) {
     	var self = this;
-    	// Selector to select from the module context.
-    	$(".class", self.$ctx);
-    	
-    	
+    	    	
         callback();
     },
     after: function() { 
