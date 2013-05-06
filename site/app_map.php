9cc9d37cefab71cf72af9b899b022e3a5080244e
Jakub Werner <jack@ubilix.com>
2013-05-06 14:56:55 +0200

    app_map.php

---
+++ site/app_map.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+	<?php include("head_tag.php")?>
+	<body>
+		
+		<!-- Navigation -->
+		<div id="header">
+			<?php include("js/terrific/Header/header.php")?>
+		</div>
+
+		
+		<!-- Container -->
+		<div class="container-fluid">
+			<?php include("js/terrific/Map/map.php")?>
+		</div><!-- Container -->
+		
+		<div id="footer">
+			<?php include ("js/terrific/Footer/footer.php") ?>
+		</div>
+		
+	</body>
+</html>
