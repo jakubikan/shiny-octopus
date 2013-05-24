780064272f9c7f7d81673d1e33364e5a067ca97e
Jakub Werner <jack@ubilix.com>
2013-05-24 17:33:28 +0200

    switched to play 2.1

--- project/Build.scala
+++ project/Build.scala
@@ -1,22 +1,21 @@
 import sbt._
 import Keys._
-
 import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "shiny-octopus"
-    val appVersion      = "1.0"
+  val appName         = "shiny-octopus2"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      javaCore,
-      javaJdbc,
-      javaEbean
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
-    )    
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
 
 }
-            
