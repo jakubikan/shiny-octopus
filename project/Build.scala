97c6bb5767f2c274935bc24634dcc44f8da39f65
Patrick Roßnagel <paddyrossnagel@aol.com>
2013-05-29 15:32:16 +0200

    mySQL database works.

--- project/Build.scala
+++ project/Build.scala
@@ -12,9 +12,8 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
+  	"mysql" % "mysql-connector-java" % "5.1.18",
   	"commons-io" % "commons-io" % "2.3"
-  	"mysql" % "mysql-connector-java" % "5.1.18"
-    
     
   )
 
