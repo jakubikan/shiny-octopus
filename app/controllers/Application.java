45142101db165d0c3813827d48d80400fd9ea323
Jakub Werner <jack@ubilix.com>
2013-05-31 21:58:50 +0200

    removed Log statements

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -48,34 +48,18 @@ public class Application extends Controller {
 				m = c.getDeclaredMethod("render", String.class);
 				content = (Html) m.invoke(null, page);
 			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 			
 			try {
 				m = c.getDeclaredMethod("render");
 				content = (Html) m.invoke(null);
 			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 			
