955c57bc57dbfd5b8857d95bd4a152c079850dea
unknown <jobublin@3DLABOR14.in.fhkn.de>
2013-06-19 15:16:20 +0200

    alle Daten vom Master auf play umgezogen. Läuft noch nicht sauber

---
+++ app/assets/javascripts/ContextMenuRoute/js/Tc.Module.ContextMenuRoute.js
@@ -0,0 +1,18 @@
+(function($) {
+	Tc.Module.ContextMenuRoute = Tc.Module.ContextMenu.extend({
+		after : function() {
+			var self = this;
+			$("[data-id='remove-route']",self.$ctx).bind("click",function(e) {
+				self.fire("removeRoute",self.currentWorkingObject,function(){});	
+				e.stopImmediatePropagation();
+				self.closeContextMenu.call(this,self);		
+			});
+			$("[data-id='route-marker-switch']",self.$ctx).bind("click",function(e) {
+				self.fire("switchRouteToMarkers",self.currentWorkingObject,function(){});		
+				e.stopImmediatePropagation();
+				self.closeContextMenu.call(this,self);	
+			});
+		}
+
+	});
+})(Tc.$);
