1e5f9e73e222bd2312121be31e5551b298839460
Jakub Werner <jack@ubilix.com>
2013-06-19 15:56:28 +0200

    main fixed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,13 +1,25 @@
-(function($) {
-    $(document).ready(function() {
-        var $page = $('body'),
-            application = new Tc.Application($page);
-
-        application.registerModules();
-        application.start();
-		
-    });
-})(Tc.$);
-
+require([
+         "helper",
+         "helper2",
+         "Appmenu/js/Tc.Module.Appmenu", 
+         "ContextMenu/js/Tc.Module.ContextMenu", 
+         "Footer/js/Tc.Module.Footer", 
+         "Header/js/Tc.Module.Header", 
+         "Map/js/Tc.Module.Map", 
+         "Waypoint/js/Tc.Module.Waypoint", 
+         "Weather/js/Tc.Module.Weather"         
+], function(l) {
+	(function($) {
+	    $(document).ready(function() {
+	        var $page = $('body'),
+	            application = new Tc.Application($page);
+	
+	        application.registerModules();
+	        application.start();
+			
+	    });
+	    
+	})(Tc.$);
+});
 
 
